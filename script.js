7934e2195a1a9f0bf2a2557c30e27dfe0b9e19e1
alexraikiu <alexraikiu18@gmail.com>
2026-07-21 11:55:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Música
+const music = document.getElementById(`bgMusic`);
+const label = document.getElementById(`audio-label`);
+let playing = false;
+
+function tryAutoplay () {
+    const p = music.play();
+    if (p !== undefined){
+        p.then(() => {playing = true ; label.textContent = `pausar`; })
+            .catch(() => {
+                document.addEventListener(`click`, () => {
+                    music.play().then(() => { playing  = true; label.textContent = `Pausar`; });
+                }, { once: true });
+            });
+    }
+}
+
+function toggleMusic() {
+  if (playing) { music.pause(); playing = false; label.textContent = 'Música'; }
+  else         { music.play();  playing = true;  label.textContent = 'Pausar'; }
+}
+
+tryAutoplay();
